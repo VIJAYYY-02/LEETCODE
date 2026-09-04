46b373ccf3f179670cfcce4e2aa287e04a6f7296
VJ <vijaysinghrajput.020207@gmail.com>
2026-09-04 17:21:07 +0530

    Time: 0 ms (100%), Space: 95.7 MB (30.17%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -5,27 +5,44 @@ public:
         int n2 = nums2.size();
         int n = n1 + n2;
 
-        // Merge arrays into one
-        vector<int> arr(n);
-        for (int i = 0; i < n1; i++) {
-            arr[i] = nums1[i];
-        }
-        for (int j = 0; j < n2; j++) {
-            arr[n1 + j] = nums2[j];
-        }
 
-        // // Bubble sort 
-        // for (int i = 0; i < n - 1; i++) {
-        //     for (int j = 0; j < n - i - 1; j++) {
-        //         if (arr[j] > arr[j + 1]) {
-        //             int temp = arr[j];
-        //             arr[j] = arr[j + 1];
-        //             arr[j + 1] = temp;
-        //         }
-        //     }
+        vector<int> arr;
+        arr.reserve(n);
+
+        int i = 0, j = 0;
+
+        // Merge two sorted arrays
+        while (i < n1 && j < n2) {
+            if (nums1[i] < nums2[j]) {
+                arr.push_back(nums1[i++]);
+            } else {
+               arr.push_back(nums2[j++]);
+            }
+        }
+        while (i < n1) arr.push_back(nums1[i++]);
+        while (j < n2) arr.push_back(nums2[j++]);
+
+        // // Merge arrays into one
+        // vector<int> arr(n);
+        // for (int i = 0; i < n1; i++) {
+        //     arr[i] = nums1[i];
         // }
-         sort(arr.begin(),arr.end());
-        // Median calculation using (n % 2 == 1)
+        // for (int j = 0; j < n2; j++) {
+        //     arr[n1 + j] = nums2[j];
+        // }
+
+        // // // Bubble sort 
+        // // for (int i = 0; i < n - 1; i++) {
+        // //     for (int j = 0; j < n - i - 1; j++) {
+        // //         if (arr[j] > arr[j + 1]) {
+        // //             int temp = arr[j];
+        // //             arr[j] = arr[j + 1];
+        // //             arr[j + 1] = temp;
+        // //         }
+        // //     }
+        // // }
+        //  sort(arr.begin(),arr.end());
+        // // Median calculation using (n % 2 == 1)
         if (n % 2 == 1) {
             // Odd length → middle element
             return arr[n / 2];
